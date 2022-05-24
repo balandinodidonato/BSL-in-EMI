a3596cd07e457fc4a3a2de83ead0543356d35209
Balandino Di Donato <info@balandinodidonato.com>
2022-05-24 11:48:46 +0100

    updates gode

--- phase-2/analysis/motion/code/merger.py
+++ phase-2/analysis/motion/code/merger.py
@@ -1,7 +1,8 @@
-# Created by Balandino Di Donato as part of the BSL in EMI project funded by Creative Informatics
-# This script merges body, face and hand data into a single json file.
+# This script merges all json file from two folders, one with face+body data and with body+hand dta into a JSON file 
 
-import json, sys
+# Dependecies
+import glob, json
+import sys
 
 # Gather file path
 participant = str(sys.argv[1])
@@ -9,21 +10,22 @@ song = str(sys.argv[2])
 face_file_path = sys.argv[3] # Path to face data set
 hand_file_path = sys.argv[4] # Path to face data set
 
-# Opening JSON file
-face_file = open(face_file_path)
-hand_file = open(hand_file_path)
-
-# Returns JSON object as a dictionary
-face_data = json.load(face_file)
-hand_data = json.load(hand_file)
-  
 # Lists and variables
 pose_keypoints_2d = []
 face_keypoints_2d = []
 hand_left_keypoints_2d = []
 hand_right_keypoints_2d = []
 data_list = []
-participantNo = 1
+face_data = []
+hand_data = []
+
+for f in glob.glob(face_file_path+"/*.json"):
+    with open(f,) as infile:
+        face_data.append(json.load(infile))
+
+for f in glob.glob(hand_file_path+"/*.json"):
+    with open(f,) as infile:
+        hand_data.append(json.load(infile))
 
 # iterates through the JSON with the face data and extracts the face keypoints
 for object in face_data:
@@ -37,10 +39,6 @@ for object in hand_data:
         hand_left_keypoints_2d.append(people["hand_left_keypoints_2d"])
         hand_right_keypoints_2d.append(people["hand_right_keypoints_2d"])
 
-# Closing file
-face_file.close()
-hand_file.close()
-
 # tests on wether the two JSON contains the same number of objects
 if(len(face_data)==len(face_data)):
     for frame in range(len(face_data)):
@@ -48,8 +46,8 @@ if(len(face_data)==len(face_data)):
         f_data = face_keypoints_2d[frame]
         hl_data = hand_left_keypoints_2d[frame]
         hr_data = hand_right_keypoints_2d[frame]
-        data_list.append({"participant":participantNo, "song":song, "frame":frame, "pose_keypoints_2d":body, "face_keypoints_2d":f_data, "hand_left_keypoints_2d":hl_data, "hand_right_keypoints_2d":hr_data})
+        data_list.append({"participant":participant, "song":song, "frame":frame, "pose_keypoints_2d":body, "face_keypoints_2d":f_data, "hand_left_keypoints_2d":hl_data, "hand_right_keypoints_2d":hr_data})
 
-    filename = song + "_" + participant + ".json"
+    filename = "../data/" + song + "_" + participant + ".json"
     with open(filename, 'w') as f:
         json.dump(data_list, f)
