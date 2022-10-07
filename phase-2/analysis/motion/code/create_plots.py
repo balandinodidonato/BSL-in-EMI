8bdc0fed479b95a57a80d64d765d1f675e9db127
Balandino Di Donato <info@balandinodidonato.com>
2022-10-07 16:12:08 +0100

    adds save immages functions

--- phase-2/analysis/motion/code/create_plots.py
+++ phase-2/analysis/motion/code/create_plots.py
@@ -4,6 +4,10 @@ import sys
 from matplotlib import pyplot as plt
 
 filepath = str(sys.argv[1])
+plots = str(sys.argv[2])
+participant_no = str(sys.argv[3])
+
+my_dpi = 100
 
 # Opening JSON file
 with open(filepath, 'r') as f:
@@ -35,16 +39,21 @@ for keypoint in original_data['data']:
     keypoints_qom.append(keypoint['keypoints_fod']['distance_total'])
     keypoints_qom_angle.append(keypoint['keypoints_fod']['angle_total'])
 
-print('frames: '+str(len(frame_count)))
+plt.figure(figsize=(2000/my_dpi, 720/my_dpi), dpi=my_dpi)
 
-plt.title('Features')
+plt.title('Participant no. '+participant_no)
 plt.xlabel('Frames')
 plt.ylabel('Pixels')
 
-plt.plot(frame_count, keypoints_qom, label='Overall QoM')
-plt.plot(frame_count, keypoints_wrists_distance, label='Wrist distances')
-plt.plot(frame_count, keypoints_RWrist_nose_distance, label='R wrist-nose distance')
-plt.plot(frame_count, keypoints_LWrist_nose_distance, label='L wrist-nose distance')
+if plots == 'qom':
+    plt.plot(frame_count, keypoints_qom, label='Overall QoM')
+
+elif plots == 'wrists':
+    plt.plot(frame_count, keypoints_wrists_distance, label='Wrist distances')
+    plt.plot(frame_count, keypoints_RWrist_nose_distance, label='R wrist-nose distance')
+    plt.plot(frame_count, keypoints_LWrist_nose_distance, label='L wrist-nose distance')
+
+
 plt.legend()
-plt.show()
+plt.savefig('./plot/'+plots+'_P'+participant_no+'.png', bbox_inches='tight')
 
