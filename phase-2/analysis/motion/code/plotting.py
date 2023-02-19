3dd76f2ab6fdf56f207a73436ded2f7b119d513e
Balandino Di Donato <info@balandinodidonato.com>
2023-02-19 13:32:18 +0000

    adds edits to colour graphs

--- phase-2/analysis/motion/code/plotting.py
+++ phase-2/analysis/motion/code/plotting.py
@@ -4,61 +4,83 @@ import sys
 from plottingFunctions import *
 import numpy as np
 
-plots = str(sys.argv[1])
-#part = int(sys.argv[2])
-
-
-loadJSONFiles()
+plots = int(sys.argv[1])
 songsDeltas = []
 songsJointsDelta = []
 songsJointsNames = []
+songsFrames = []
+shoulderX = []
+rawWristShoulders = []
 index = 0
+dataFrames = []
+songRaw = []
 
-for index in range(6):
-    songsJointsNames.append(jointNames)
+if plots < 7:
+    loadJSONFiles()
 
-for songsData in songsFeatures:
-    songsDeltas.append(songsData['deltaTotal'])
-    songsJointsDelta.append(songsData['deltaTotalJoints'])
-
-#print(songsDeltas)
-if plots == '0':
     for index in range(6):
-        plt.bar(SPstringRed[index], songsDeltas[index], color='b')
+        songsJointsNames.append(jointNames)
+
+    for songsData in songsFeatures:
+        songsDeltas.append(songsData['deltaTotal'])
+        songsJointsDelta.append(songsData['deltaTotalJoints'])
+        dataFrames.append(songsData['data'])
+
+    for rawDataSong in dataFrames:
+        rawFramesDataSong = []
+        for rawFramesData in rawDataSong['data']:
+            rawFramesDataSong.append(rawFramesData['raw_wrists_shoulders'])
+        songRaw.append(rawFramesDataSong)
+
+    print(songRaw[0][0])
+
+  #  print(rawWristShoulders[0])
+
+
+# prints delta total of songs
+if plots == 0:
+    for index in range(6):
         plt.ylim(0, 70000000)
-        plt.yticks([0, 2000000, 4000000, 6000000, 7000000])
+        plt.yticks([0, 10000000, 20000000, 30000000, 40000000, 50000000, 60000000, 70000000])
+        plt.bar(SPstringRed[index], songsDeltas[index], color=colors[index])
+        plt.grid(visible=True, axis='y', alpha=0.5)
         plt.title('Total delta') 
         plt.ylabel('Keypoint Delta / Frame')
         plt.xlabel('Interpretation')
     plt.savefig('./plot/deltaTotalParticipant.png')
+    plt.show()
 
-if plots == '1':
+# prints 
+if plots == 1:
     figure, axis = plt.subplots(6, 1, figsize=(7,10))
     
     for data in songsJointsDelta:
-        axis[index].bar(jointID, data)
+        axis[index].bar(jointID, data, color=rgb01)
         axis[index].set_title(SPstring[index]) 
         axis[index].set_ylim(0, 8000000)
         axis[index].set_yticks([0, 2000000, 4000000, 6000000, 8000000])
+
         axis[index].set_ylabel('Kpnt Delta / Frame')
         axis[index].set_xlabel('Keypoints')
         index += 1
         index %= 6    
-   
+
+    plt.title('Interpreations delta') 
     plt.subplots_adjust(left=0.1, bottom=0.05, right=0.95, top=0.95, wspace=0.2, hspace=1)
     plt.savefig('./plot/deltaTotalJoints.png')
+    plt.show()
 
     
-if plots == '2':
+if plots == 2:
     x = jointID
     y = np.array(songsJointsDelta)
 
-    plt.bar(x, y[0], color='r')
-    plt.bar(x, y[1], bottom=y[0], color='g')
-    plt.bar(x, y[2], bottom=y[0]+y[1], color='b')
-    plt.bar(x, y[3], bottom=y[0]+y[1]+y[2], color='orangered')
-    plt.bar(x, y[4], bottom=y[0]+y[1]+y[2]+y[3], color='limegreen')
-    plt.bar(x, y[5], bottom=y[0]+y[1]+y[2]+y[3]+y[4], color='deepskyblue')
+    plt.bar(x, y[0], color=colors[0])
+    plt.bar(x, y[1], bottom=y[0], color=colors[1])
+    plt.bar(x, y[2], bottom=y[0]+y[1], color=colors[2])
+    plt.bar(x, y[3], bottom=y[0]+y[1]+y[2], color=colors[3])
+    plt.bar(x, y[4], bottom=y[0]+y[1]+y[2]+y[3], color=colors[4])
+    plt.bar(x, y[5], bottom=y[0]+y[1]+y[2]+y[3]+y[4], color=colors[5])
 
     plt.ylim(0, 35000000)
     plt.ylabel('Keypoint Delta / Frame')
@@ -66,19 +88,7 @@ if plots == '2':
     plt.legend(SPstringRed)
     plt.savefig('./plot/deltaTotalJointsStack_All.png')
 
-if plots == '3':
-    plt.bar(jointID, songsJointsDelta[part-1], label=SPstringRed[part-1], color='r')
-    plt.bar(jointID, songsJointsDelta[part+2], label=SPstringRed[part+2], bottom=songsJointsDelta[part-1], color='b')
-    plt.ylabel('Keypoint Delta / Frame')
-    plt.xlabel('Keypoint No.')
-    plt.ylim(0, 15000000)
-    plt.grid(visible=True, axis='y', alpha=0.5)
-
-    plt.legend()
-    plt.savefig('./plot/deltaTotalJointsStack_S12_P'+str(part)+'.png')
-
-
-if plots == '4':
+if plots == 4:
 
     figure, axis = plt.subplots(3, 1, figsize=(7,9))
 
@@ -99,27 +109,31 @@ if plots == '4':
     plt.savefig('./plot/deltaTotalJointsStack_Participants.png')
 
 
-if plots == '5':
-    figure, axis = plt.subplots(2, 1, figsize=(7,9))
+if plots == 5:
+    figure, axis = plt.subplots(2, 1, figsize=(9,7))
     y = np.array(songsJointsDelta)
 
-    axis[0].bar(jointID, y[0], label='P1', color='r')
-    axis[0].bar(jointID, y[1], label='P2', bottom=y[0], color='g')
-    axis[0].bar(jointID, y[2], label='P3', bottom=y[0]+y[1], color='b')
+    axis[0].bar(jointID, y[0], label='P1', color=rgb01)
+    axis[0].bar(jointID, y[1], label='P2', bottom=y[0], color=rgb01*0.6)
+    axis[0].bar(jointID, y[2], label='P3', bottom=y[0]+y[1], color=rgb01*0.3)
 
-    axis[1].bar(jointID, y[3], label='P1', color='r')
-    axis[1].bar(jointID, y[4], label='P2', bottom=y[3], color='g')
-    axis[1].bar(jointID, y[5], label='P3', bottom=y[3]+y[4], color='b')
+    axis[1].bar(jointID, y[3], label='P1', color=rgb01)
+    axis[1].bar(jointID, y[4], label='P2', bottom=y[3], color=rgb01*0.6)
+    axis[1].bar(jointID, y[5], label='P3', bottom=y[3]+y[4], color=rgb01*0.3)
     
     for index in range(2):
        # axis[index].set_label(songsTitle)
         axis[index].legend()
-        axis[index].set_title(songsTitle[index]) 
+        axis[index].set_title(songsTitleFull[index]) 
         axis[index].set_ylim(0, 18000000)
         axis[index].set_yticks([0, 2000000, 4000000, 6000000, 8000000, 10000000, 12000000, 14000000, 16000000, 18000000])
-        axis[index].set_ylabel('Kpnt Delta / Frame')
+        axis[index].set_ylabel('Delta (Pixesls)')
         axis[index].set_xlabel('Keypoints')
         axis[index].grid(visible=True, axis='y', alpha=0.5)
 
-    plt.subplots_adjust(left=0.1, bottom=0.05, right=0.95, top=0.95, wspace=0.2, hspace=0.3) 
+    #plt.title('Particiapnts\' Kpnt Delta vs Songs')
+    plt.subplots_adjust(left=0.1, bottom=0.1, right=0.95, top=0.95, wspace=0.2, hspace=0.3) 
     plt.savefig('./plot/deltaTotalJointsStack_Songs.png')
+    plt.show()
+
+#if plots == 6:
--- phase-2/analysis/motion/code/plottingFunctions.py
+++ phase-2/analysis/motion/code/plottingFunctions.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 songsFeatures = []
 participants = ['P1', 'P2', 'P3']
@@ -21,7 +22,11 @@ SPstringRed = [
             'S2 P3'
             ]
 
-songs = ['Apparat_Goodbye', 'DaftPunk_Crescendools']
+colors = ['r', 'g', 'b', 'orangered', 'limegreen', 'deepskyblue']
+
+songsTitleFull = ['Goodbye by Apparat', 'Crescendools by DaftPunk']
+
+songsJ = ['Apparat_Goodbye', 'DaftPunk_Crescendools']
 keypoints_IDs = [['0',  "Nose"], ['1',  "Neck"], ['2',  "RShoulder"], ['3',  "RElbow"], ['4',  "RWrist"], ['5',  "LShoulder"], ['6',  "LElbow"], ['7',  "LWrist"], ['8',  "MidHip"], ['9',  "RHip"],['10', "RKnee"], ['11', "RAnkle"], ['12', "LHip"], ['13', "LKnee"], ['14', "LAnkle"], ['15', "REye"], ['16', "LEye"], ['17', "REar"], ['18', "LEar"], ['19', "LBigToe"], ['20', "LSmallToe"], ['21', "LHeel"], ['22', "RBigToe"], ['23', "RSmallToe"], ['24', "RHeel"]]
 jointNames = []
 jointID = []
@@ -31,10 +36,13 @@ for k_string in keypoints_IDs:
     jointNames.append(k_string[1])
 
 def loadJSONFiles():
-    for indextSong in songs:
+    for indextSong in songsJ:
         for indexPart in participants:
             with open('data/'+indextSong+'_'+indexPart+'_features.json', 'r') as f:
                 songsFeatures.append(json.load(f))
     
     print('Message: JSON files loaded')
     
+rgb255 = [[245, 66, 132],[255, 0, 0],[247, 95, 0],[247, 193, 0],[247, 247, 0],[181, 247, 0],[103, 247, 0],[0, 255, 0],[247, 45, 0],[178, 102, 255],[51, 255, 51],[255, 255, 255],[102, 178, 255],[51, 153, 255],[51, 51, 255],[255, 0, 127],[178, 102, 255],[255, 0, 255],[102, 0, 204],[0, 0, 255],[0, 0, 255],[51, 51, 255],[0, 255, 255],[0, 255, 255],[0, 255, 255],[255, 255, 255]]
+
+rgb01 = np.array(rgb255)/255
